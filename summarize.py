2f61f1db33e51fcf37b789fcfad60799529215bd
Sergey Polivin <s.polivin@gmail.com>
2025-03-31 14:41:07 +0200

    Improvements for the summarization script

--- summarize.py
+++ summarize.py
@@ -1,220 +1,229 @@
-"""Script for generating summaries of PDF-articles from Arxiv."""
+"""
+Scientific Articles Summarizer Script
+=====================================
+
+This script processes a PDF file, summarizes its content using a Hugging Face model, and generates a report.
+
+In case of both given config and cli arguments, cli arguments override those given in a config file.
+
+Usage:
+    python summarize.py input.pdf --config=config.json --report-name=summ_report_lora.txt
+
+Arguments:
+    filename (str): Path to the PDF article to be summarized.
+    --config (str, optional): Path to the config JSON file.
+    --model-path (str, optional): Path to a Hugging Face summarization model.
+    --tokenizer-path (str, optional): Path to a Hugging Face summarization model tokenizer.
+    --max-output-tokens (int, optional): Maximum number of output tokens.
+    --min-output-tokens (int, optional): Minimum number of output tokens.
+    --num-beams (int, optional): Number of beams for beam search algorithm.
+    --length-penalty (int, optional): Penalty for the summary length.
+    --repetition-penalty (int, optional): Penalty for repetitive words.
+    --no-repeat-ngram-size (int, optional): Avoiding repetitive phrases.
+    --lora-adapters-path (str, optional): Path to LoRA adapters
+    --line-width (int, optional): Maximum line width for report formatting.
+    --max-keywords-num (int, optional): Maximum number of keywords in the summary report.
+    --report-name (str, optional): Name of the output summary report.
+    --save-folder (str, optional): Folder to save the generated summary.
+"""
 
 import argparse
-import re
-import textwrap
-import warnings
-from pathlib import Path
+import json
 
-import fitz
-import nltk
-import torch
-from peft import AutoPeftModelForSeq2SeqLM
-from transformers import AutoTokenizer, logging
+from deep_compend import ArticleSummarizer, SummaryGenerationConfig
 
-# Selecting GPU if available
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# Path to the base model on HuggingFace
-CKPT_PATH = "facebook/bart-large-cnn"
-# Path to LoRA adapters of the fine-tuned model
-HF_REPO_PATH = "spolivin/bart-arxiv-lora"
+# Defaults to be automatically used if no CLI args provided
+BUILT_IN_DEFAULTS = {
+    "min_output_tokens": 30,
+    "max_output_tokens": 250,
+    "num_beams": 4,
+    "length_penalty": 1.0,
+    "repetition_penalty": 1.2,
+    "no_repeat_ngram_size": 3,
+    "model_path": "google-t5/t5-small",
+    "tokenizer_path": None,
+    "lora_adapters_path": None,
+    "line_width": 100,
+    "max_keywords_num": 5,
+    "report_name": "summary_report.txt",
+    "save_folder": "summaries",
+}
 
-# Suppressing warnings
-warnings.filterwarnings("ignore")
-logging.set_verbosity_error()
-# Downloading additional NLTK-packages
-nltk.download("punkt_tab", quiet=True)
-nltk.download("punkt", quiet=True)
 
-# Parsing scripts arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("filepath", help="Path to the PDF-article to be summarized")
+def load_config(config_path: str) -> dict:
+    """Loads JSON file.
+
+    Args:
+        config_path (str): Path to a JSON file to be loaded.
+
+    Returns:
+        dict: Dict-representation of JSON file or empty dict in case of unspecified/non-existent JSON file.
+    """
+    # Returning empty dict in case no JSON file is given
+    if not config_path:
+        print("Warning: Config file not specified. Using built-in defaults.\n")
+        return {}
+    try:
+        # Loading JSON contents if file exists
+        with open(config_path) as f:
+            print(f"Loading config from '{config_path}'...\n")
+            return json.load(f)
+    except FileNotFoundError:
+        # Returning empty dict in case of non-existent JSON file
+        print(
+            "Warning: Config file '{config_path}' not found. Using built-in defaults.\n"
+        )
+        return {}
+
+
+# Defining an Arguments Parser
+parser = argparse.ArgumentParser(
+    description="Summarize a PDF article using a Hugging Face model."
+)
+
+# Positional argument: PDF file path
 parser.add_argument(
-    "-mot",
+    "filepath", type=str, help="Path to the PDF article to be summarized"
+)
+
+# Other optional arguments
+parser.add_argument(
+    "-c", "--config", type=str, help="Path to the config JSON file"
+)
+parser.add_argument(
+    "-mp",
+    "--model-path",
+    type=str,
+    help="Path to summarization model",
+)
+parser.add_argument(
+    "-tp",
+    "--tokenizer-path",
+    type=str,
+    help="Path to summarization model tokenizer",
+)
+parser.add_argument(
+    "-mxot",
     "--max-output-tokens",
-    help="Maximum number of output tokens",
     type=int,
-    default=250,
+    help="Maximum number of output tokens",
 )
 parser.add_argument(
-    "-stt",
-    "--save-to-txt",
-    help="Save summary to txt-file",
-    type=bool,
-    default=False,
+    "-mnot",
+    "--min-output-tokens",
+    type=int,
+    help="Minimum number of output tokens",
 )
+parser.add_argument(
+    "-nb", "--num-beams", type=int, help="Number of beams for beam search"
+)
+parser.add_argument(
+    "-lp",
+    "--length-penalty",
+    type=float,
+    help="Penalty for the summary length",
+)
+parser.add_argument(
+    "-rp",
+    "--repetition-penalty",
+    type=float,
+    help="Penalty for repetitive words",
+)
+parser.add_argument(
+    "-nrns",
+    "--no-repeat-ngram-size",
+    type=int,
+    help="Avoid repetitive phrases",
+)
+parser.add_argument(
+    "-lap", "--lora-adapters-path", type=str, help="Path to LoRA adapters"
+)
+parser.add_argument(
+    "-lw",
+    "--line-width",
+    type=int,
+    help="Maximum line width for report formatting",
+)
+parser.add_argument(
+    "-mkn",
+    "--max-keywords-num",
+    type=int,
+    help="Maximum number of keywords in the summary report",
+)
+parser.add_argument(
+    "-rn",
+    "--report-name",
+    type=str,
+    help="Name of the output summary report",
+)
+parser.add_argument(
+    "-sf",
+    "--save-folder",
+    type=str,
+    help="Folder to save the generated summary",
+)
+
+# Parsing the arguments
 args = parser.parse_args()
 
+# Load config file
+config = load_config(config_path=args.config)
 
-def extract_main_text(pdf_path):
-    """
-    Extracts the main body text from an article, starting from the 'Introduction'
-    section (or its variations) and stopping before the 'References' section.
+# Retrieving entered CLI arguments
+cli_args = {k: v for k, v in vars(args).items() if v is not None}
 
-    :param text: The full text of the article
-    :type text: str
-    :raises ValueError: Exception raised if the input file is not a PDF
-    :return: Extracted main body text
-    :rtype: str
-    """
-    # Validating the input file
-    if ".pdf" not in pdf_path:
-        raise ValueError("Input file should have 'pdf' extension.")
-    # Retrieving the article text
-    doc = fitz.open(pdf_path)
-    text = ""
-    for page in doc:
-        text += page.get_text("text") + "\n"
-
-    # Defining regex pattern for Introduction-like sections
-    intro_pattern = re.compile(
-        r"(?:^|\n)\s*(?:\d+\.?\s*)?(Introduction|Background|Overview|Intro|The Trends)\b.*?\n",
-        re.IGNORECASE,
-    )
-
-    # Defining regex pattern for Reference-like section
-    references_pattern = re.compile(
-        r"(?:^|\n)\s*(?:\d+\.?\s*)?(References|Bibliography|Works Cited)\b",
-        re.IGNORECASE,
-    )
-
-    # Searching for the introduction section
-    intro_match = intro_pattern.search(text)
-    start_index = intro_match.start() if intro_match else 0
-
-    # Searching for the references section
-    references_match = references_pattern.search(text, start_index)
-    end_index = references_match.start() if references_match else len(text)
-
-    # Extracting main content of the article
-    main_text = text[start_index:end_index].strip()
-
-    return main_text
-
-
-def clean_text(text):
-    """
-    Removes numeric/author-year citations and cleans the input text
-
-    :param text: The full text of the article
-    :type text: str
-    :return: Cleaned main body text
-    :rtype: str
-    """
-    # Removing numeric citations: e.g. [4], [3,5,8]
-    text = re.sub(r"\[\d+(?:,\s*\d+)*\]", "", text)
-
-    # Removing author-year citations: e.g (Doe et al., 2020)
-    text = re.sub(r"\(\s*[A-Z][a-z]+(?:\s+et al\.)?,\s*\d{4}\s*\)", "", text)
-
-    # Removing extra spaces left after cleanup
-    text = re.sub(r"\s{2,}", " ", text).strip()
-    # Removing non-word characters
-    text = re.sub(r"\s+", " ", text)
-    # Removing spaces left before punctuation
-    text = re.sub(r"\s+([.,!?])", r"\1", text)
-
-    return text.strip()
-
-
-def summarize_text(
-    model,
-    tokenizer,
-    text,
-    device=DEVICE,
-    max_input_length=1024,
-    max_output_length=args.max_output_tokens,
-):
-    """
-    Tokenizes input sequence and generates summary as output.
-
-    :param model: Transformers Model object
-    :param tokenizer: Transformers Tokenizer
-    :param text: Article text
-    :type text: str
-    :param device: Device to be used for summary generation, defaults to DEVICE
-    :type device: torch.device, optional
-    :param max_input_length: Maximum context window, defaults to 1024
-    :type max_input_length: int, optional
-    :param max_output_length: Maximum number of generated tokens as summary, defaults
-        to args.max_output_tokens
-    :type max_output_length: int, optional
-    :return: Formatted generated summary of the article
-    :rtype: str
-    """
-    # Tokenizing input sequence
-    inputs = tokenizer(
-        text, return_tensors="pt", truncation=True, max_length=max_input_length
-    ).to(device)
-    # Moving model to device
-    model = model.to(device)
-    # Generating tokens as output
-    with torch.no_grad():
-        summary_ids = model.generate(
-            **inputs, max_length=max_output_length, num_beams=4, early_stopping=True
-        )
-    # Decoding and prettifying output
-    raw_summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-
-    return prettify_summary(raw_summary)
-
-
-def prettify_summary(summary):
-    """
-    Fixes the format of the generated summary.
-
-    :param summary: The summary generated by the model.
-    :type summary: str
-    :return: Prettified generated summary.
-    :rtype: str
-    """
-    # Spliting input into sentences and capitalizing each one
-    sentences = nltk.tokenize.sent_tokenize(summary)
-    prettified_summary = " ".join(s.capitalize() for s in sentences)
-
-    # Removing unwanted spaces before punctuation
-    prettified_summary = re.sub(r"\s+([.,!?])", r"\1", prettified_summary)
-
-    return prettified_summary
+# Collecting the final configuration to be used for summary generation
+final_config = (
+    {**BUILT_IN_DEFAULTS, **config, **cli_args}
+    if args.config and len(cli_args) >= 2
+    else {**BUILT_IN_DEFAULTS, **cli_args}
+)
 
 
 if __name__ == "__main__":
-    # Retrieving and preprocessing the article text
-    pdf_path = args.filepath
-    print(f"Extracting article text from '{pdf_path}'...")
-    article_text = extract_main_text(pdf_path=pdf_path)
-    article_text_cleaned = clean_text(article_text)
-    print("Text extracted and preprocessed")
+    # Echoing the arguments/values to be used for summarization
+    print("Using configuration:")
+    for key, value in final_config.items():
+        print(f"{key} => {value}")
+    print()
 
-    # Loading tokenizer and model for summarization
-    print(f"Loading tokenizer from '{CKPT_PATH}'...")
-    tokenizer = AutoTokenizer.from_pretrained(CKPT_PATH)
-    print(f"Loading model from '{HF_REPO_PATH}'...")
-    lora_model = AutoPeftModelForSeq2SeqLM.from_pretrained(HF_REPO_PATH)
-    print("Tokenizer and model loaded")
-
-    # Running summarization
-    print(f"Running summarization of the article from '{pdf_path}'...")
-    lora_summary = summarize_text(
-        model=lora_model, tokenizer=tokenizer, text=article_text_cleaned
+    # Defining a summarization generation configuration
+    summ_config = SummaryGenerationConfig(
+        min_length=final_config["min_output_tokens"],
+        max_length=final_config["max_output_tokens"],
+        num_beams=final_config["num_beams"],
+        length_penalty=final_config["length_penalty"],
+        repetition_penalty=final_config["repetition_penalty"],
+        no_repeat_ngram_size=final_config["no_repeat_ngram_size"],
     )
-    print("Summarization finished")
 
-    # Saving summary in txt-file
-    if args.save_to_txt:
-        # Wrapping lines of the output summary
-        wrapped_lines = textwrap.wrap(lora_summary, width=130)
-        # Building filename of the output summary
-        match = re.search(r"([^/]+)\.pdf$", pdf_path)
-        filename = f"{match.group(1)}_summarized.txt"
-        # Defining filepath to the summary
-        p = Path("summaries")
-        p.mkdir(exist_ok=True)
-        filepath = p / filename
-        # Writing summary line by line to file
-        with filepath.open("w", encoding="utf-8") as file:
-            for line in wrapped_lines:
-                file.write(line.ljust(130) + "\n")
-        print(f"Summary saved to '{str(filepath)}'")
-    else:
-        print(lora_summary)
+    # Instantiating an object for summarization
+    print("Loading tokenizer and model for summarization...")
+    arxiv_summarizer = ArticleSummarizer(
+        model_path=final_config["model_path"],
+        tokenizer_path=final_config["tokenizer_path"],
+    )
+
+    # Optionally attaching LoRA adapters to the model specified
+    print("Tokenizer and model loaded")
+    if final_config.get("lora_adapters_path"):
+        print("Attaching LoRA adapters...")
+        arxiv_summarizer.load_lora_adapters(
+            lora_adapters_path=final_config["lora_adapters_path"]
+        )
+        print("LoRA adapters attached")
+
+    # Generating summary of the text
+    print("Running summarization...")
+    summary = arxiv_summarizer.summarize_text(
+        pdf_path=final_config["filepath"], config=summ_config
+    )
+
+    # Generating a summary report
+    print("Generating report...")
+    arxiv_summarizer.generate_summary_report(
+        filename=final_config["report_name"],
+        linewidth=final_config["line_width"],
+        kwrds_num=final_config["max_keywords_num"],
+        save_folder=final_config["save_folder"],
+    )
