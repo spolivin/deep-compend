a9ee5e3a59b4ccc372ebeab7afd6f284c789ee8c
Sergey Polivin <s.polivin@gmail.com>
2025-04-07 22:42:24 +0200

    Introduced additional CLI arguments to test scripts

--- scripts/run_bart_lora.sh
+++ scripts/run_bart_lora.sh
@@ -1,2 +1,5 @@
 ARTICLE_PATH="${1}"
-python summarize.py "$ARTICLE_PATH" --config=configs/bart_lora_config.json
+python summarize.py "$ARTICLE_PATH" \
+    --config=configs/bart_lora_config.json \
+    --min-keywords-length=5 \
+    --max-keywords-num=7
--- scripts/run_default.sh
+++ scripts/run_default.sh
@@ -1,2 +1,4 @@
 ARTICLE_PATH="${1}"
-python summarize.py "$ARTICLE_PATH"
+python summarize.py "$ARTICLE_PATH" \
+    --min-keywords-length=7 \
+    --max-keywords-num=10
